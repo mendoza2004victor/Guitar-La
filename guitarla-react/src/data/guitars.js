2b5f86f85063f91892f0b046dad3f844ea28d799
Victor Eduado Mendoza Velasquez <victormendoz8@gmail.com>
2025-08-01 00:24:30 -0600

    .

--- guitarla-react/src/data/guitars.js
+++ guitarla-react/src/data/guitars.js
@@ -1,86 +1,24 @@
 export default [
   {
     id: 1,
-    name: "Lukather",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt",
-    price: $299,
+    name: "Guitarra Acústica",
+    description: "Sonido cálido y natural.",
+    price: 500,
     image: "img/guitarra_01.jpg",
   },
   {
     id: 2,
-    name: "SRV",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt",
-    price: $299,
+    name: "Guitarra Eléctrica",
+    description: "Potente y versátil.",
+    price: 700,
     image: "img/guitarra_02.jpg",
   },
     {
     id: 3,
-    name: "BORLAND",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt",
-    price: $299,
+    name: "Guitarra Flamenca",
+    description: "Cuerpo ligero, sonido brillante.",
+    price: 650,
     image: "img/guitarra_03.jpg"
-  },
-    {
-    id: 4,
-    name: "VAI",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt",
-    price: $299,
-    image: "img/guitarra_04.jpg"
-  },
-    {
-    id: 5,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_05.jpg"
-  },
-    {
-    id: 6,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_06.jpg"
-  },
-    {
-    id: 7,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_07.jpg"
-  },
-    {
-    id: 8,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_08.jpg"
-  },
-    {
-    id: 9,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_09.jpg"
-  },
-    {
-    id: 10,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_10.jpg"
-  },
-    {
-    id: 11,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_11.jpg"
-  },
-    {
-    id: 12,
-    name: "Guitarra Flamenca",
-    description: "Cuerpo ligero, sonido brillante.",
-    price: 650,
-    image: "img/guitarra_12.jpg"
   }
+  
 ];
